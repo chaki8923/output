75b3ea568274c0da9a6b4d8d630a87f56fc2375a
chaki8923 <74690893+chaki8923@users.noreply.github.com>
2021-01-29 13:55:42 +0900

    Add files via upload

---
+++ mypage.php
@@ -0,0 +1,22 @@
+<?php
+
+require('head.php');
+require('header.php');
+
+?>
+   
+
+   
+<body>
+<div class="myleft">
+<form action="" method="post" class="output">
+   <input type="date" name="date">
+   <input type="time" name="time">
+   <input type="text" name="text" placeholder="タイトル">
+   <textarea name="comment" id="" cols="80" rows="60"></textarea>
+   <input type="submit" value="登録">
+    
+</form>
+</div>
+</body>
+</html>
